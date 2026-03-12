0fa26cff13020318e05b58dc775330f53d999a5b
Nandhinisivakumar27 <nandhinisivakumar.dev@gmail.com>
2026-03-12 22:48:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function addTask(){
+
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if(task === ""){
+        alert("Enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    let list = document.getElementById("taskList");
+    list.appendChild(li);
+
+    input.value = "";
+}
